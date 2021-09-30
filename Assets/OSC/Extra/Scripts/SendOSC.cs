8a73192c56fb692ea470922ba8c33ec98c588191
mikebru <32722658+mikebru@users.noreply.github.com>
2021-09-29 19:46:32 -0500

    Initial commit

---
+++ Assets/OSC/Extra/Scripts/SendOSC.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class SendOSC : MonoBehaviour {
+
+	public OSC oscReference;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+    public void SendMessageScene(int index)
+    {
+        OscMessage message = new OscMessage();
+        message.address = "/Scene";
+        message.values.Add(index);
+        oscReference.Send(message);
+    }
+
+
+
+
+    /*
+    void OnMouseDown() {
+        OscMessage message = new OscMessage();
+        message.address = "/test";
+        message.values.Add(transform.position.x);
+        message.values.Add(transform.position.y);
+        message.values.Add(transform.position.z);
+        oscReference.Send(message);
+	}*/
+
+}
